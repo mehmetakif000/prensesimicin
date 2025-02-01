c153c3ed7b7e2f92224104e309ed061762968dba
mehmetakif000 <mhmt.akfalp29@gmail.com>
2025-02-01 16:26:47 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "biz <span class=\"digit\">" + days + "</span> gün <span class=\"digit\">" + hours + "</span> saat <span class=\"digit\">" + minutes + "</span> dakika <span class=\"digit\">" + seconds + "</span> saniye"; 
 	$("#clock").html(result);
 }
